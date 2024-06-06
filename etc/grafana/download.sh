d2270a9c5470750fc1296a3dd1256f56e48b337a
runalsh <21105673+runalsh@users.noreply.github.com>
2024-06-07 00:43:19 +0300

    upd

--- etc/grafana/download.sh
+++ etc/grafana/download.sh
@@ -12,7 +12,8 @@ if [[ -f ${VM_DS_PATH}/plugin.json ]]; then
 fi
 
 rm -rf ${VM_DS_PATH}/* || true
-mkdir -p ${VM_DS_PATH}
+mkdir -p /var/lib/grafana/plugins/victorialogs-datasource
+mkdir -p /var/lib/grafana/plugins/victoriametrics-datasource
 
 export LATEST_VERSION=$(curl https://api.github.com/repos/VictoriaMetrics/victorialogs-datasource/releases/latest | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | head -1); \
 curl -L https://github.com/VictoriaMetrics/victorialogs-datasource/releases/download/${LATEST_VERSION}/victorialogs-datasource-${LATEST_VERSION}.tar.gz -o ${PLUGIN_PATH}/victorialogs-plugin.tar.gz && \

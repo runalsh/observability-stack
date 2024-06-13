26adf50aeb47e13450e5c7fc4908be83dd4467d6
runalsh <21105673+runalsh@users.noreply.github.com>
2024-06-13 19:48:38 +0300

    upd producer2.py +add exceptions

--- LABS/kafka/producer2.py
+++ LABS/kafka/producer2.py
@@ -10,23 +10,45 @@ from kafka import KafkaProducer
 servers=['localhost:9091', 'localhost:9092', 'localhost:9093']
 topic = 'topic'
 
-producer = KafkaProducer(bootstrap_servers=servers)
-producer = KafkaProducer()
+# producer = KafkaProducer(bootstrap_servers=servers)
+# producer = KafkaProducer()
+
+# def gendata():
+
+#     faker = Faker()
+
+#     data = {'id' : str(uuid.uuid4()), 'timestamp': str(datetime.datetime.now()), 'name': faker.name(), 
+#     'country': faker.country(), 'job': faker.job(), 'image': faker.image_url()}
+#     prod = KafkaProducer(bootstrap_servers=servers,value_serializer = lambda x:dumps(x).encode('utf-8'))
+#     print(data)
+#     prod.send(topic=topic, value=data)
+#     prod.flush()
 
 def gendata():
-
     faker = Faker()
 
-    data = {'id' : str(uuid.uuid4()), 'timestamp': str(datetime.datetime.now()), 'name': faker.name(), 
-    'country': faker.country(), 'job': faker.job(), 'image': faker.image_url()}
-    prod = KafkaProducer(bootstrap_servers=servers,value_serializer = lambda x:dumps(x).encode('utf-8'))
-    print(data)
-    prod.send(topic=topic, value=data)
-    prod.flush()
+    data = {
+        'id': str(uuid.uuid4()),
+        'timestamp': str(datetime.datetime.now()),
+        'name': faker.name(),
+        'country': faker.country(),
+        'job': faker.job(),
+        'image': faker.image_url()
+    }
+
+    try:
+        prod = KafkaProducer(bootstrap_servers=servers, value_serializer=lambda x: dumps(x).encode('utf-8'))
+        prod.send(topic=topic, value=data)
+        prod.flush()
+        print(f"{data}")
+    except TimeoutError as e:
+        print(f"Failed to send data to Kafka: {e}")
+    except Exception as e:
+        print(f"Failed to send data to Kafka: {e}")    
 
 if __name__ == "__main__":
     gendata()
-    schedule.every(1).seconds.do(gendata)
+    schedule.every(0.1).seconds.do(gendata)
 
     while True:
         schedule.run_pending()
